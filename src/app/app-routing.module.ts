f0b2e19f7caab165b8bc76ae0ea2035e54ed1349
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-16 16:23:59 -0500

    Add children routes to auth section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 const routes: Routes = [
+  /* {
+    path: 'auth',
+    loadChildren: () => import( './auth/auth.module' ).then( m => m.AuthModule )
+  }, */
+  {
+    path: 'auth',
+    loadChildren: () => import ( './auth/auth.module' ).then( m => m.AuthModule )
+  },
   {
     path: '404',
     component: ErrorPageComponent,
--- src/app/shared/error-page/error-page.component.ts
+++ src/app/shared/error-page/error-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.scss']
+  styleUrls: []
 })
 export class ErrorPageComponent implements OnInit {
 
