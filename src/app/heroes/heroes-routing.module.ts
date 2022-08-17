c2b67cc226016dffe0cb685995d3f45493a2896b
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-17 00:17:48 -0500

    Fix children routes bug

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,25 +1,22 @@
-// Modules
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-// Components
 import { AddHeroComponent } from './pages/add-hero/add-hero.component';
 import { HeroComponent } from './pages/hero/hero.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
-
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'list',
         component: ListComponent
       },
       {
-        path: 'add',
+        path: 'add-hero',
         component: AddHeroComponent
       },
       {
@@ -43,9 +40,13 @@ const routes: Routes = [
 ]
 
 
+
 @NgModule({
   imports: [
     RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class HeroesRoutingModule { }
