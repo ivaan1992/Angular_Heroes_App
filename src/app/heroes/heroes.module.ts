6402aaf73c12ca2f9d4a2c90fa0125a7dab6b4c0
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-17 10:07:38 -0500

    Add flex layout module in heroes module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { NgModule } from '@angular/core';
 
@@ -21,6 +22,7 @@ import { SearchComponent } from './pages/search/search.component';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ],
   exports: [
