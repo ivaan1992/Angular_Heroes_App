c7aea6e478fd685a5c3724358df103ab7ffa334e
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-17 12:14:49 -0500

    Create a service that shows all heroes info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 // Components
@@ -18,6 +19,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
