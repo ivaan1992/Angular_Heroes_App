cba31f605b6af6d77733b061d94de9468a961644
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-18 14:05:44 -0500

    Create a method to update the heroes

--- src/app/heroes/pages/add-hero/add-hero.component.ts
+++ src/app/heroes/pages/add-hero/add-hero.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Publisher, Superhero } from '../../interfaces/heroes.interfaces';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-add-hero',
@@ -30,9 +32,18 @@ export class AddHeroComponent implements OnInit {
     alt_img: ''
   }
 
-  constructor( private heroesService: HeroesService ) { }
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+    .pipe (
+      switchMap( ({ id }) => this.heroesService.getHeroById( id ) )
+    )
+    .subscribe( ( hero => this.hero = hero ) );
+
+
   }
 
   saveInfo() {
@@ -41,11 +52,16 @@ export class AddHeroComponent implements OnInit {
       return;
     }
 
+    if( this.hero.id ) {
+      //Update hero
 
-    this.heroesService.addHero( this.hero )
-    .subscribe( resp => {
-      console.log( 'ans', resp );
-    })
+      this.heroesService.updateHero( this.hero )
+      .subscribe( hero => console.log( 'Updating:', hero ) )
+    } else {
+      this.heroesService.addHero( this.hero )
+      .subscribe( resp => {
+        console.log( 'Answer:', resp )
+      });
+    }
   }
-
 }
